f33f78ba2215cc776346ce5d9b6a2c1fb49a2abe
Jonatas Ferreira <silvajonatas2@gmail.com>
2021-04-19 20:38:16 -0300

    criação do repositorio / fundamentos nodejs

---
+++ fundamentos-nodejs/src/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+
+app.get("/", (req, res) => {
+  return res.json({message: "Hello World! Ignite :D"});
+});
+
+//localhost:3333
+app.listen(3333);
