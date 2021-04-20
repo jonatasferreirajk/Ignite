090de524baa3a551516cfc0d19cd797625f197bd
Jonatas Ferreira <silvajonatas2@gmail.com>
2021-04-20 14:49:29 -0300

    tipos de parametros

--- fundamentos-nodejs/src/index.js
+++ fundamentos-nodejs/src/index.js
@@ -2,11 +2,35 @@ const express = require('express');
 
 const app = express();
 
-app.get("/", (req, res) => {
-  return res.json({message: "Hello World!! Ignite :D"});
+app.use(express.json());
+
+app.get("/courses", (req, res) => {
+  const query = req.query;
+  console.log(query);
+  return res.json(["Curso 1","Curso 2"]);
 });
 
-//localhost:3333
+app.post("/courses", (req, res) =>{
+  const body = req.body;
+  console.log(body);
+  return res.json(["Curso 1","Curso 2", "Curso 3"]);
+})
+
+app.put("/courses/:id", (req, res) =>{
+  const {id} = req.params;
+  console.log(id);
+  return res.json(["Curso 4","Curso 2", "Curso 3"]);
+})
+
+app.patch("/courses/:id", (req, res) =>{
+  return res.json(["Curso 4","Curso 5", "Curso 3"]);
+})
+
+app.delete("/courses/:id", (req, res) =>{
+  return res.json(["Curso 4","Curso 5"]);
+})
+
+
 app.listen(3333, () =>{
   console.log("🚀Back-End Started! Server is On!");
 });
