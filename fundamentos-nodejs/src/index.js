b00c0d9c79890add6c6e046379ecdef75a567eb6
Jonatas Ferreira <silvajonatas2@gmail.com>
2021-04-19 21:12:04 -0300

    adicionando o nodemon na aplicação

--- fundamentos-nodejs/src/index.js
+++ fundamentos-nodejs/src/index.js
@@ -3,8 +3,10 @@ const express = require('express');
 const app = express();
 
 app.get("/", (req, res) => {
-  return res.json({message: "Hello World! Ignite :D"});
+  return res.json({message: "Hello World!! Ignite :D"});
 });
 
 //localhost:3333
-app.listen(3333);
+app.listen(3333, () =>{
+  console.log("🚀Back-End Started! Server is On!");
+});
