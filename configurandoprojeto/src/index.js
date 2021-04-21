196ccb02c72a1203b3ae36130d1ee295879258db
Jonatas Ferreira <silvajonatas2@gmail.com>
2021-04-20 21:03:10 -0300

    listando extrato

--- configurandoprojeto/src/index.js
+++ configurandoprojeto/src/index.js
@@ -1,8 +1,39 @@
 const express = require('express');
+const {v4: uuidv4 } = require('uuid');
 
 const app = express();
 
+const customers = [];
+
 app.use(express.json());
+
+app.post("/account", (req, res)=>{
+  const {cpf , name} = req.body;
+
+  const customerAlreadyExists = 
+  customers.some((customer) => customer.cpf === cpf);
+
+  if(customerAlreadyExists){
+    return res.status(400).json({error: 'Customer already exists'});
+  }
+
+  customers.push({
+    cpf,
+    name,
+    id : uuidv4(),
+    statement: [],
+  });
+
+  res.status(201).send();
+});
+
+app.get("/statement/:cpf", (req, res) =>{
+  const {cpf} = req.params;
+
+  const customer = customers.find(customer => customer.cpf === cpf);
+
+  return res.json(customer.statement);
+});
 
 app.listen(3333, ()=>{
   console.log("🚀Back-End Started! Server is On!");
