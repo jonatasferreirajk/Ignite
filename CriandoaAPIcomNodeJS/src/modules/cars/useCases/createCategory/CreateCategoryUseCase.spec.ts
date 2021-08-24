2a92a0745deed14fb92e88eb47454b5766148f92
Jonatas Ferreira <silvajonatas2@gmail.com>
2021-08-24 20:11:12 -0300

    Criando o primeiro teste

---
+++ CriandoaAPIcomNodeJS/src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,15 @@
+describe("Create Category", () => {
+  it("Waiting the sum of 2 + 2 = 4", () => {
+    const sum = 2 + 2;
+    const result = 5;
+
+    expect(sum).toBe(result);
+  });
+
+  it("Waiting the sum of 2 + 2 not to be 4", () => {
+    const sum = 2 + 2;
+    const result = 5;
+
+    expect(sum).not.toBe(result);
+  });
+});
